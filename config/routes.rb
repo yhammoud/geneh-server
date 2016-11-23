a1aa07b5ae590ba7c26dced20ecaa05f01af56c5
Youssef Hammoud <yhammoud93@gmail.com>
2016-11-22 21:17:32 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'api#get_exchange_rate'
   get '/api/exchange_rate' => 'api#get_exchange_rate'
 
   get '/notifications/new' => 'notifications#new'
